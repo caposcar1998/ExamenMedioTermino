0235162b9cef1e0ed1acfba532cceb05e48dab27
Manue Ortiz Hernández <manuoh09@gmail.com>
2021-04-15 16:25:50 -0500

    Final Commit

--- src/RegexToNFA.java
+++ src/RegexToNFA.java
@@ -23,7 +23,6 @@ public class RegexToNFA {
         return iNode;
     }
 
-
     public NodeNFA union(NodeNFA iNode, NodeNFA fNode){
         int sizeFromUnion = iNode.getStates().size() + fNode.getStates().size() + 2;
         NodeNFA unionNFA = new NodeNFA(sizeFromUnion);
@@ -54,19 +53,15 @@ public class RegexToNFA {
 
     public NodeNFA star(NodeNFA iNode){
         NodeNFA starNFA = new NodeNFA(iNode.getStates().size() + 2);
-        //Sabemos que al ser star, puede existir un estado vacio
         starNFA.addPath(0,1,'ñ');
-        //Acceder a los caminos actuales del iNode (elemento antes de la *) para modificarlos de acuerdo a lo necesario
         for(NodeNFA.Paths p: iNode.getPaths()){
             int initialPath = p.getInitialState() + 1;
             int nextPath = p.getNextState() + 1;
             starNFA.addPath(initialPath, nextPath, p.getTransitionWith());
         }
-        //Conectamos el finalState de iNode hacia el nuevo final que se genera por un starNFA
+
         starNFA.addPath(iNode.getStates().size(), iNode.getStates().size() + 1, 'ñ');
-        //Se genera el 'loop' que genera la estrella, regresando al elemento anterior
         starNFA.addPath(iNode.getStates().size(), 1, 'ñ');
-        //Se agrega transición vacía del primer estado al último estado
         starNFA.addPath(0, iNode.getStates().size() + 1, 'ñ');
         starNFA.setFinalState(iNode.getStates().size(), 1);
         return starNFA;
@@ -128,7 +123,6 @@ public class RegexToNFA {
             String operatorToAttend = op.getOperators().pop();
             validateOperators(operatorToAttend, op, initialNode, finalNode);
         }
-        //return op.getActualNFA().pop();
     }
 
 
